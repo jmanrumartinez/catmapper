66a8ac440757bbb70ea629a1d5556dc25f1937f9
Joan Manrubia <jmanrumartinez@gmail.com>
2025-07-30 13:57:44 -0400

    refactor: remove NavigationLink component and simplify NavigationBar by eliminating unused navigation items

--- src/components/shared/navigation/NavigationBar.tsx
+++ src/components/shared/navigation/NavigationBar.tsx
@@ -26,12 +26,6 @@ export const NavigationBar = ({ onConnect, account }: NavigationBar) => {
   return (
     <nav className="w-full">
       <div className="mx-auto max-w-7xl py-8 flex items-center justify-between">
-        <ul className="flex items-center list-none">
-          <NavigationLink>Buy</NavigationLink>
-          <NavigationLink>Sell</NavigationLink>
-          <NavigationLink>Rent</NavigationLink>
-        </ul>
-
         <div className="flex justify-center items-center">
           <Image
             src="logo.svg"
