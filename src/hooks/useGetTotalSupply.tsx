2801b3dae9b00e4f46205fa47ea2f58dabcee9a4
Joan Manrubia <jmanrumartinez@gmail.com>
2025-07-31 10:42:57 -0400

    refactor: update useGetTotalSupply hook to return an object with totalSupply and loading state

--- src/hooks/useGetTotalSupply.tsx
+++ src/hooks/useGetTotalSupply.tsx
@@ -1,12 +1,15 @@
 import RealEstateAbi from "@consts/abis/RealEstate.json";
 import { useReadContract } from "wagmi";
 
-export const useGetTotalSupply = (): number => {
-  const { data } = useReadContract({
+export const useGetTotalSupply = (): {
+  totalSupply: number;
+  isLoading: boolean;
+} => {
+  const { data, isLoading } = useReadContract({
     abi: RealEstateAbi,
     address: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
     functionName: "totalSupply",
   });
 
-  return data as number;
+  return { totalSupply: data as number, isLoading };
 };
