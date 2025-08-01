aefd5cb39f7fa81234c2bf7486041a9a41b556b4
Joan Manrubia <jmanrumartinez@gmail.com>
2025-08-01 12:25:04 -0400

    feat: add environment variables for Reown and Alchemy, configure Sepolia network in Hardhat, and implement deployment script for RealEstate and Escrow contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,10 +4,18 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.20",
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    }
+  },
   paths: {
     sources: "./src/contracts",
     tests: "./src/contracts/__tests__",
     cache: "./cache",
-    artifacts: "./artifacts"
+    artifacts: "./artifacts",
   }
 };
