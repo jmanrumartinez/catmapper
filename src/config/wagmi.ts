da90b0163935e3d7aa4b8cce0c32204dc5a3c56e
Joan Manrubia <jmanrumartinez@gmail.com>
2025-08-01 18:51:45 -0400

    refactor: remove console log for environment variable in wagmi configuration

--- src/config/wagmi.ts
+++ src/config/wagmi.ts
@@ -4,11 +4,6 @@ import { hardhat, sepolia } from "wagmi/chains";
 const chain =
   process.env.NEXT_PUBLIC_USE_HARDHAT === "true" ? hardhat : sepolia;
 
-console.log(
-  "process.env.NEXT_PUBLIC_USE_HARDHAT",
-  process.env.NEXT_PUBLIC_USE_HARDHAT
-);
-
 const config = getDefaultConfig({
   appName: "Catmapper",
   projectId: process.env.NEXT_PUBLIC_REOWN_PROJECT_ID ?? "",
